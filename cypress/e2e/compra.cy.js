a6fa82f24cb289d93e5e2d13ce829317dfcce1cd
Adriany caroline <adrianycaroline133@gmail.com>
2025-11-05 18:40:13 -0300

    Adicionados cenários: remover produto, atualizar quantidade e checkout sem campos obrigatórios

--- cypress/e2e/compra.cy.js
+++ cypress/e2e/compra.cy.js
@@ -51,4 +51,91 @@ describe('Funcionalidade: Compra de Produto', () => {
         .should('be.visible')
         .and('contain', 'Obrigado. Seu pedido foi recebido.')
     })
+
+    it('Não deve permitir finalizar a compra sem preencher campos obrigatórios', () => {
+      cy.get('.product-block').first().click()
+    
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.button-variable-item-White').click({ force: true })
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.single_add_to_cart_button').click()
+    
+      cy.get('.woocommerce-message > .button').click()
+      cy.get('.checkout-button').click()
+    
+      // Intencionalmente não preenche os campos obrigatórios
+      cy.get('#place_order').click()
+    
+      // Validação: verificar mensagens de erro
+      cy.get('.woocommerce-error')
+        .should('be.visible')
+        .and('contain', 'Nome')
+        .and('contain', 'Sobrenome')
+        .and('contain', 'Endereço')
+        .and('contain', 'Cidade')
+        .and('contain', 'CEP')
+        .and('contain', 'Telefone')
+        .and('contain', 'Endereço de e-mail')
+    })
+
+    it('Deve remover um produto do carrinho com sucesso', () => {
+      cy.get('.product-block').first().click()
+    
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.button-variable-item-White').click({ force: true })
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.single_add_to_cart_button').click()
+    
+      cy.get('.woocommerce-message')
+        .should('contain', 'foi adicionado no seu carrinho')
+    
+      cy.get('.woocommerce-message > .button').click()
+    
+      // Remove o produto do carrinho
+      cy.get('.remove > .fa').click()
+    
+      // Validação: o carrinho deve ficar vazio
+      cy.get('.cart-empty')
+        .should('be.visible')
+        .and('contain', 'Seu carrinho está vazio.')
+    })
+
+    it('Deve atualizar a quantidade de um produto no carrinho', () => {
+      cy.get('.product-block').first().click()
+    
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.button-variable-item-White').click({ force: true })
+      cy.get('.button-variable-item-XS').click({ force: true })
+      cy.get('.single_add_to_cart_button').click()
+    
+      cy.get('.woocommerce-message')
+        .should('contain', 'foi adicionado no seu carrinho')
+    
+      cy.get('.woocommerce-message > .button').click()
+    
+      // Captura o valor do produto unitário
+      cy.get('.cart_item .product-price .woocommerce-Price-amount bdi')
+        .invoke('text')
+        .then((priceText) => {
+          const precoUnitario = parseFloat(priceText.replace('R$', '').replace(',', '.'))
+    
+          // Altera a quantidade para 2
+          cy.get('.quantity input.qty').clear().type('2')
+    
+          // Atualiza o carrinho
+          cy.get('[name="update_cart"]').click()
+      
+          // Aguarda a atualização do carrinho
+          cy.get('.woocommerce-message', { timeout: 10000 })
+            .should('contain', 'Carrinho atualizado')
+    
+          // Calcula o subtotal
+          cy.get('.cart-subtotal .woocommerce-Price-amount bdi')
+            .invoke('text')
+            .then((subtotalText) => {
+              const subtotal = parseFloat(subtotalText.replace('R$', '').replace(',', '.'))
+                expect(subtotal).to.be.closeTo(precoUnitario * 2, 0.5)
+              })
+        })
+    })
 })
